bcfb0fa885b814ccbe3308c724c5c2493beac4de
Imrumi-1111 <upasanachakraborty18@gmail.com>
2023-04-26 22:51:38 +0530

    26th

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [hiddenName, setHiddenName] = useState('********');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setHiddenName(name);
+    setName('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, my name is : {hiddenName}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
