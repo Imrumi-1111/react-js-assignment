7423710aa6af3ab06c053726a7f3ba8fbd30e8dd
Imrumi-1111 <upasanachakraboty18@gmail.com>
2023-05-04 23:10:21 +0530

    4thmayassignmentdog

--- src/components/dog.js
+++ src/components/dog.js
@@ -1,24 +1,26 @@
+import axios from "axios";
 import "./dog.css"
 
 import { useState, useEffect } from 'react'
 
 export default function DogImages(){
     const [images, setImages] = useState('');
+    
     async function getDogData (){
-        try{
-            const response = await fetch('https://dog.ceo/api/breeds/image/random')
-            const data = await response.json()
-            setImages(data.message)
+       
+       try{
+            const response = await axios.get('https://dog.ceo/api/breeds/image/random' , getDogData)
+            //const data = await response.json()
+            setImages(response.data.message)
         }
         catch(error){
             console.log(error)
         }
-        
+           
     }
     useEffect(() =>{
         getDogData();
     },[])
-
 
     return(
         <div>
