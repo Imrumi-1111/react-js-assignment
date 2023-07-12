6bbb18b0d29f5a3df22ec4f524c6650e71a95ea0
Imrumi-1111 <upasanachakraboty18@gmail.com>
2023-07-12 13:33:07 +0530

    emojipicker

---
+++ src/component/main/main.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+import EmojiPicker from 'emoji-picker-react';
+
+export default function Main(){
+    
+    return(
+        <Fragment>
+           <EmojiPicker/>
+        </Fragment>
+    )
+}
